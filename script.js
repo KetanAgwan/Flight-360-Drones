3fa834654edda680e8f6830e0f8a43ddb77cd1c5
Ketan Agwan <ketanagwan7597@gmail.com>
2023-08-31 09:53:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// this function is to move the slider automatically 
+const slideArr = document.querySelectorAll(".radio");
+counter = 1;
+setInterval(()=>{
+    document.getElementById("radio"+counter).checked = true;
+    counter++;
+    if(counter > 3) counter =1;
+},10000)
+
+console.log(document.getElementById("radio1"));
